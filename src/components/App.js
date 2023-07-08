887a026c335e0c7a1c4ce797a9f8e7796b3d3695
ryanngu023 <ryannguyen10023@gmail.com>
2023-07-07 19:05:19 -0700

    initial react setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      {/* NavBar
+          Header if needed */}
+      
+    </div>
+  );
+}
+
+export default App;
