c2275c3a3ef3f1eedd7fccc3b594b285f5c01ea8
ryanngu023 <ryannguyen10023@gmail.com>
2023-07-07 22:19:14 -0700

    clean up and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/home.js';
+import About from './pages/about.js';
+import FAQ from './pages/faq.js';
 
 function App() {
   return (
     <div>
       {/* NavBar
           Header if needed */}
-      
+          <main>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="faq" element={<FAQ />} />
+            </Routes>
+          </main>
     </div>
   );
 }
---
+++ src/components/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home(props) {
+    return (
+        <div>
+            <p>Hi, my name is kieto</p>
+            <p> !loremresidofbdsiougbfsouhgbfsuhosfhou</p>
+        </div>
+    )
+}
