462c16c179cbaafe3d8dbe3dc1b5ace8b7c02868
TophTheTofu <122495765+TophTheTofu@users.noreply.github.com>
2023-07-20 13:53:08 -0700

    Update with rough draft of side guide page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home.js';
 import About from './pages/about.js';
 import FAQ from './pages/faq.js';
 import Navbar from './Navbar.js';
+import SizeGuide from "./pages/sizeguide.js";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="faq" element={<FAQ />} />
+          <Route path="sizeguide" element={<SizeGuide />} />
         </Routes>
       </main>
     </div>
