df880663ca003c73830f4bcce4355b76d12a154f
ryanngu023 <ryannguyen10023@gmail.com>
2023-07-31 23:59:58 -0700

    testing contact forms, schemas, and css adjustments

--- src/components/Assistance.js
+++ src/components/Assistance.js
@@ -1,20 +1,15 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
 export default function Assistance(props) {
+
     return (
         <div className="contactbody">
             <div className='textbox'>
                 <h2>Regular Assistance</h2>
                 <p>Use this form to get in touch with us with any questions, concerns, or issues that you may have!</p>
             </div>
-            <form className="helpform">
-                <input type='text' name='name' placeholder="Name"></input>
-                <input type='text' name='email' placeholder="Email"></input>
-                <input type='text' name='phonenum' placeholder="Phone Number"></input>
-                <textarea name='message' placeholder="Enter your question, concern or issue."></textarea>
-                <input id='uploadmedia' type='file' name='attachments'></input>
-                <input id='sendbutton' type='submit' value='Send'></input>
-            </form>
+            <ContactForm />
         </div>
     )
 }
--- src/components/Custom.js
+++ src/components/Custom.js
@@ -1,9 +1,76 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
 export default function Custom(props) {
     return (
-        <div>
+        <div className="contactbody">
+            <div className='textbox'>
+                <h2>Custom Order</h2>
+                <p>Custom order can be requested upon this page.
+                    Personal images can be uploaded so that our 
+                    digitizing manager can assess whether the image
+                    is reasonable enough to embroider. Follow the
+                    guidelines below for choosing an image.</p>
+            </div>
+            <div className="textbox custom_steps">
+                <div>
+                    <div>
+                        <p>(1)</p>
+                        <p>You send the product design below, and I will confirm with you via email if it’s possible to create or not.</p>
+                    </div>
+                    <img src='/img/Footer Logo.png' alt='Process of sending product design through form' />
+                </div>
+
+                <div>                    
+                    <img src='/img/Footer Logo.png' alt='Process of sending product design through form' />
+                    <div>
+                        <p>(2)</p>
+                        <p>Talk thru email / zoom call (20 minute max)</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <p>(3)</p>
+                        <p>We will create one free visual sample based on your request. if the customer likes it, then they can pay the price of the product.</p>
+                    </div>
+                    <img src='/img/Footer Logo.png' alt='Process of sending product design through form' />
+                </div>
+
+                <div>                    
+                    <img src='/img/Footer Logo.png' alt='Process of sending product design through form' />
+                    <div>
+                        <p>(4)</p>
+                        <p>We will revise the product until finalization.</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <p>(5)</p>
+                        <p>Product gets shipped out! :D</p>
+                    </div>
+                    <img src='/img/Footer Logo.png' alt='Process of sending product design through form' />
+                </div>
+
+                <div>                    
+                    <img src='/img/Footer Logo.png' alt='Process of sending product design through form' />
+                    <div>
+                        <p>(6)</p>
+                        <p>Customer received product. is happy. leaves a positive review on the website.</p>
+                    </div>
+                </div>
+
+                <div className='examples'>
+                    <p>GOLDEN STANDARD</p>
+                </div>
+
+                <div className='examples'>
+                    <p>POOR STANDARD</p>
+                </div>
+            </div>
             
+            <ContactForm />
         </div>
     )
 }
